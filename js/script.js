44b33e77066cbfa2c1fde0f937c54f0eab16a304
RED-BENA <dlekrud0503@naver.com>
2018-01-16 15:21:06 +0900

    #5 Abstract common features of Header to mixin and Add header

--- js/script.js
+++ js/script.js
@@ -25,15 +25,15 @@ $(document).ready(function() {
   $('html, .js-slide-menu__header__close, .js-slide-menu__header__logo').click(function(e) {
     // 메뉴 닫기
     e.stopPropagation();
-    $('.js-slide-menu').slideUp();
+    $('.js-slide-menu').slideUp(200);
     $('.js-dim-bg').hide();
     $('html').css('overflow', 'scroll');
   });
 
-  $('.js-index__header__hamburger').click(function(e) {
+  $('.js-header__hamburger').click(function(e) {
     // 메뉴 열기
     e.stopPropagation();
-    $('.js-slide-menu').slideDown();
+    $('.js-slide-menu').slideDown(200);
     $('.js-dim-bg').show();
     $('html').css('overflow', 'hidden');
   })
