eb10dd1e89c7a9b5b31e9d8b673bd0b00715681f
RED-BENA <dlekrud0503@naver.com>
2018-01-16 09:35:35 +0900

    #3 Add transition day and night

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
+  var scrollBottom = 0;
+  var day = Math.round($('.js-reasonable-saving__title').offset().top+$('.js-reasonable-saving__title').height());
+  var night = Math.round($('.js-loan__title').offset().top+$('.js-loan__title').height());
   $(document).scroll(function() {
-    // console.log($(this).scrollTop());
+    scrollBottom = $(this).scrollTop()+$(window).height();
+
+    // console.log(scrollBottom);
+
+    if (night <= scrollBottom) { // 일정 스크롤이 넘어가면 (아래)
+      $('.js-loan--night').addClass('active');
+    }
+
+    if (day >= scrollBottom) { // 일정 스크롤이 넘어가면 (위)
+      $('.js-loan--night').removeClass('active');
+    }
   })
 });
