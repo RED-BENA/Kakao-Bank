abb23cfb737f892aea2b1dc63f3047c9bda17852
RED-BENA <dlekrud0503@naver.com>
2018-01-16 10:48:59 +0900

    #3 Add star shining animation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   var scrollBottom = 0;
-  var day = Math.round($('.js-reasonable-saving__title').offset().top+$('.js-reasonable-saving__title').height());
+  var day = Math.round($('.js-reasonable-saving__title').offset().top);
   var night = Math.round($('.js-loan__title').offset().top+$('.js-loan__title').height());
   $(document).scroll(function() {
     scrollBottom = $(this).scrollTop()+$(window).height();
@@ -11,12 +11,14 @@ $(document).ready(function() {
       $('.js-loan--night').addClass('active'); // 하늘이 어두워짐
       $('.js-loan__sun').addClass('shadow'); // 태양에 그림자 생김
       $('.js-loan__moon').addClass('rise'); // 달이 뜸
+      $('.js-loan__star').addClass('rise');
     }
 
     if (day >= scrollBottom) { // 일정 스크롤이 넘어가면 (위)
       $('.js-loan--night').removeClass('active');
       $('.js-loan__sun').removeClass('shadow');
       $('.js-loan__moon').removeClass('rise');
+      $('.js-loan__star').removeClass('rise');
     }
   })
 });
