3ee01f7f519ec5b48a59a305c276682db34b4c8c
RED-BENA <dlekrud0503@naver.com>
2018-01-16 10:17:52 +0900

    #3 Add Sun and Moon transition

--- js/script.js
+++ js/script.js
@@ -8,11 +8,15 @@ $(document).ready(function() {
     // console.log(scrollBottom);
 
     if (night <= scrollBottom) { // 일정 스크롤이 넘어가면 (아래)
-      $('.js-loan--night').addClass('active');
+      $('.js-loan--night').addClass('active'); // 하늘이 어두워짐
+      $('.js-loan__sun').addClass('shadow'); // 태양에 그림자 생김
+      $('.js-loan__moon').addClass('rise'); // 달이 뜸
     }
 
     if (day >= scrollBottom) { // 일정 스크롤이 넘어가면 (위)
       $('.js-loan--night').removeClass('active');
+      $('.js-loan__sun').removeClass('shadow');
+      $('.js-loan__moon').removeClass('rise');
     }
   })
 });
