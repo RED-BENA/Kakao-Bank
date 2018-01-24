9ccbdb30f432b865fc2d94985188e2718133240a
RED-BENA <dlekrud0503@naver.com>
2018-01-24 15:35:35 +0900

    #6 Add tab-menu change

--- js/notice.js
+++ js/notice.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-  
+  $('.js-tab-menu__header__item').click(function() {
+    if ($(this).index() == 0) {
+      $('.js-tab-menu__header__item').removeClass('active');
+      $(this).addClass('active');
+
+      $('.tab-menu > div').removeClass('on');
+      $('.js-tab-menu__notice').addClass('on');
+    } else if ($(this).index() == 1) {
+      $('.js-tab-menu__header__item').removeClass('active');
+      $(this).addClass('active');
+
+      $('.tab-menu > div').removeClass('on');
+      $('.js-tab-menu__event').addClass('on');
+    }
+  })
 });
