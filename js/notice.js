1e4fe29f9b9f4bb08cf8677b958331b38b954927
RED-BENA <dlekrud0503@naver.com>
2018-01-25 11:09:33 +0900

    #6 Add page-number and prev, next button

--- js/notice.js
+++ js/notice.js
@@ -13,5 +13,17 @@ $(document).ready(function() {
       $('.tab-menu > div').removeClass('on');
       $('.js-tab-menu__event').addClass('on');
     }
-  })
+  });
+
+  $('.js-tab-menu--next').click(function(e) {
+    e.preventDefault();
+    $('.js-notice__container.on').next().addClass('on');
+    $('.js-notice__container.on').prev().removeClass('on');
+  });
+
+  $('.js-tab-menu--prev').click(function(e) {
+    e.preventDefault();
+    $('.js-notice__container.on').prev().addClass('on');
+    $('.js-notice__container.on').next().removeClass('on');
+  });
 });
