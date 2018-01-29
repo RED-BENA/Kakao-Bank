4a20f3b007d9afda55bff3d4d734b614d7543245
RED-BENA <dlekrud0503@naver.com>
2018-01-29 15:16:21 +0900

    #8 Mark up Three genre question (make component later)

--- js/question-detail.js
+++ js/question-detail.js
@@ -10,22 +10,20 @@ $(document).ready(function() {
     question_count += 10;
 
     $('.js-question-detail__list__item').removeClass('active');
-    $('.js-question-detail__list__item:nth-child(-n+'+question_count+')').addClass('active');
+    $('.js-question-detail__list__item:nth-of-type(-n+'+question_count+')').addClass('active');
 
-    if ($('.js-question-detail__list__item.active').length < question_count) {
-      $(this).addClass('end');
-    }
+    // if ($('.js-question-detail__list__item.active').length < question_count) {
+    //   $(this).addClass('end');
+    // }
   });
 
   $('.js-question-detail__list__item').click(function(e) {
-
-    console.log($(this).find('.js-item-detail'));
     if ($(this).find('.js-item-detail').hasClass('active')) {
       $(this).find('.js-item-detail').removeClass('active');
-      $(this).next('.js-question-detail__list__answer').slideUp();
+      $(this).next('.js-question-detail__list__answer').slideUp(250);
     } else {
       $(this).find('.js-item-detail').addClass('active');
-      $(this).next('.js-question-detail__list__answer').slideDown();
+      $(this).next('.js-question-detail__list__answer').slideDown(250);
     }
   });
 });
