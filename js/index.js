4fba4eebb17c3aefc65cdba77537365f94bbefc7
RED-BENA <dlekrud0503@naver.com>
2018-02-09 18:37:07 +0900

    #12 Modify card slider bug

--- js/index.js
+++ js/index.js
@@ -2,14 +2,22 @@ $(document).ready(function() {
   var scrollBottom = scrollBottom = $(this).scrollTop()+$(window).height();;
   var day = Math.round($('.js-reasonable-saving__title').offset().top);
   var night = Math.round($('.js-loan__title').offset().top+$('.js-loan__title').height());
+
+  // card-slider의 width
   var slider_length = 0;
 
   // check-card__slider__item의 너비
   var card_width = $('.js-check-card__slider__item').outerWidth(true);
+
+  // card-slider의 현재 margin
   var current_margin = 0;
+
+  var clone_card = $('.js-check-card__slider__item').clone().removeClass('active');
 
   // 체크카드 슬라이더의 width 세팅
   $.setWidth = function() {
+    slider_length = 0;
+
     // 체크카드 슬라이더의 width 계산
     $('.js-check-card__slider__item').each(function() {
       slider_length += card_width;
@@ -63,22 +71,58 @@ $(document).ready(function() {
 
   // 이전 버튼을 클릭했을 때
   $('.js-check-card__prev-arrow').click(function() {
+    var new_clone_card = clone_card;
     current_margin += card_width;
 
     $('.js-check-card__slider__item.active').prev().addClass('active');
     $('.js-check-card__slider__item.active').next().removeClass('active');
 
     $('.js-check-card__slider').css('margin-left', current_margin+'px');
+
+    // 첫 카드가 active 상태일 때
+    if ($('.js-check-card__slider__item:first-child').hasClass('active')) {
+      var item_name = $('.js-check-card__slider__item:first-child').attr('class').split(' ')[2];
+      var item_number = item_name.substr(item_name.length - 1);
+
+      console.log(item_number);
+
+      if (item_number == 1) {
+        $(new_clone_card[new_clone_card.length - 1]).prependTo('.js-check-card__slider');
+      } else {
+        $(new_clone_card[item_number - 2]).prependTo('.js-check-card__slider');
+      }
+
+      $.setWidth();
+
+      $('.js-check-card__slider').css('margin-left', '0px');
+    }
   });
 
   // 다음 버튼을 클릭했을 때
   $('.js-check-card__next-arrow').click(function() {
+    clone_card = $('.js-check-card__slider__item').clone().removeClass('active');
     current_margin += card_width * -1;
 
     $('.js-check-card__slider__item.active').next().addClass('active');
     $('.js-check-card__slider__item.active').prev().removeClass('active');
 
     $('.js-check-card__slider').css('margin-left', current_margin+'px');
+
+    // 마지막 카드가 active 상태일 때
+    if ($('.js-check-card__slider__item:last-child').hasClass('active')) {
+      var item_name = $('.js-check-card__slider__item:last-child').attr('class').split(' ')[2];
+      var item_number = item_name.substr(item_name.length - 1);
+
+      if (item_number == 5) {
+        $(clone_card[0]).appendTo('.js-check-card__slider');
+      } else {
+        $(clone_card[item_number]).appendTo('.js-check-card__slider');
+      }
+
+      $.setWidth();
+
+      $('.js-check-card__slider').css('margin-left', current_margin+'px');
+    }
   })
 });
 0
