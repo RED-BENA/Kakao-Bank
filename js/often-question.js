0a9b000127be8340cf7119646bbec141d53bc4ad
RED-BENA <dlekrud0503@naver.com>
2018-02-23 16:27:07 +0900

    #15 Add answer

--- js/often-question.js
+++ js/often-question.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
   $('.often-question').addClass('open');
+
+  $('.js-often-question__list--detail__item').click(function(e) {
+    e.preventDefault();
+    $(this).toggleClass('open');
+
+    if ($(this).hasClass('open')) {
+      $('.js-often-question__list--detail__answer').slideUp(200);
+      $(this).next('.js-often-question__list--detail__answer').slideDown(200);
+    } else {
+      $(this).next('.js-often-question__list--detail__answer').slideUp(200);
+    }
+  })
 });
