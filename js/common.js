afaa0a3480de2c1c695784bf1c81c355ce231bab
RED-BENA <dlekrud0503@naver.com>
2018-02-23 15:03:32 +0900

    #16 Add event page and Modify slide menu

--- js/common.js
+++ js/common.js
@@ -1,26 +1,27 @@
 $(document).ready(function() {
   $('html, .js-slide-menu__header__close, .js-slide-menu__header__logo').click(function(e) {
     // 메뉴 닫기
+
     e.stopPropagation();
-    $('.js-slide-menu').slideUp(200);
     $('.js-dim-bg').hide();
 
-    if ($(window).width() <= 768) {
-      $('html').css('overflow', 'scroll');
+    if ($(window).width() < 1024) {
+      $('.js-slide-menu--mobile').slideUp(200);
     } else {
-
+      $('.js-slide-menu--pc').slideUp(200);
     }
   });
 
   $('.js-header__hamburger').click(function(e) {
     // 메뉴 열기
     e.stopPropagation();
-    $('.js-slide-menu').slideDown(200);
     $('.js-dim-bg').show();
 
-    if ($(window).width() <= 768) {
+    if ($(window).width() < 1024) {
+      $('.js-slide-menu--mobile').slideDown(200);
       $('html').css('overflow', 'hidden');
     } else {
+      $('.js-slide-menu--pc').slideDown(200);
       $('html').css('overflow', 'scroll');
     }
   });
